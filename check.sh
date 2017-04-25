4a97029d0bf34aadf86b0bb98a9b3ed2aae60a69
offa <bm-dev@yandex.com>
2017-04-25 17:38:50 +0200

    Check script added.

---
+++ check.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set extglob
+
+SOURCE_FILE=README.md
+
+FDROID_SETTINGS=$( \
+            grep -ni "https://f-droid.org/repository/browse/?" $SOURCE_FILE \
+            | sed "s/.*https:\/\/f-droid.org\/repository\/browse\/?//g" \
+            | sed "s/)].*/;/g" \
+            )
+
+
+FAILED=0
+
+while IFS=';' read -ra TOK; do
+    for i in "${TOK[@]}"; do
+        echo -ne "Check '$i' ... "
+
+        if [[ "$i" =~ ^fdid=([[:alnum:]]|[\._-])+$ ]];
+        then
+            echo "OK"
+        else
+            echo "INVALID"
+            let "FAILED += 1"
+        fi
+
+    done
+done <<< "$FDROID_SETTINGS"
+
+if [[ $FAILED -ge 1 ]];
+then
+    echo "Invalid Urls: $FAILED"
+    exit 1
+fi
+
